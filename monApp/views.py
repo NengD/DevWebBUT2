d5ff07c585e0864c1255e1a508502b9d945e3d7e
Desgranges Lucas <o22401316@or-iut-i207-f04.iut45.univ-orleans.fr>
2025-09-10 14:38:46 +0200

    fin tp1

--- monApp/views.py
+++ monApp/views.py
@@ -1,8 +1,23 @@
-from .app import app
+from monApp.app import app
+import config
 
 @app.route('/')
 def index():
     return "Hello world !"
+
+if __name__ == "__main__":
+    app.run()
+
+@app.route('/about/')
+def about():
+    return config.ABOUT
+
+if __name__ == "__main__":
+    app.run()
+
+@app.route('/contact/')
+def contact():
+    return config.CONTACT
 
 if __name__ == "__main__":
     app.run()
